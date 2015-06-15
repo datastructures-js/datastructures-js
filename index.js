26e72191fe5ae04bd1bae5549abea8de510a7d40
Eyas Ranjous <eyas@eyasranjous.info>
2015-06-15 09:56:29 -0700

    convert tabs to space

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var src = './src';
 
 module.exports = {
-	stack: require(src + '/stack.js'),
-	queue: require(src + '/queue.js'),
-	priorityQueue: require(src + '/priorityQueue.js'),
-	set: require(src + '/set.js'),
-	linkedList: require(src + '/linkedList.js'),
-	doublyLinkedList: require(src + '/doublyLinkedList.js'),
-	hashtable: require(src + '/hashtable.js'),
-	binarySearchTree: require(src + '/binarySearchTree.js'),
-	directedGraph: require(src + '/directedGraph.js')
+    stack: require(src + '/stack.js'),
+    queue: require(src + '/queue.js'),
+    priorityQueue: require(src + '/priorityQueue.js'),
+    set: require(src + '/set.js'),
+    linkedList: require(src + '/linkedList.js'),
+    doublyLinkedList: require(src + '/doublyLinkedList.js'),
+    hashtable: require(src + '/hashtable.js'),
+    binarySearchTree: require(src + '/binarySearchTree.js'),
+    directedGraph: require(src + '/directedGraph.js')
 }
