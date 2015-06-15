39f0f0558950ba7d7594d7dcc98a80958e534351
Eyas Ranjous <eyas@eyasranjous.info>
2015-06-15 09:52:22 -0700

    add Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+
+    // project configuration
+    grunt.initConfig({
+        jshint: {
+            options: {
+                reporter: require('jshint-stylish')
+            },
+            files: ['./src/*.js', './test/*.spec.js']
+        },
+
+        mochaTest: {
+            files: ['./test/*.spec.js']
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-mocha-test');
+
+    grunt.registerTask('build', ['jshint']);
+    grunt.registerTask('test', ['mochaTest']);
+}
