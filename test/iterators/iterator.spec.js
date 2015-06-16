c4711bfb0706a028499278ed9d65b79d1e6a8c33
Eyas Ranjous <eyas@eyasranjous.info>
2015-06-16 16:53:32 -0700

    add iterator function test

---
+++ test/iterators/iterator.spec.js
@@ -0,0 +1,23 @@
+var expect = require('chai').expect,
+    iterator = require('../../src/iterators/iterator');
+
+describe('iterator test', function() {
+
+	var elements = [1, 2, 3];
+		itr = iterator(elements);
+
+	it('should have a null current first', function() {
+		expect(itr.current()).to.be.equal(null);
+	});
+
+	it('should iterate over the elements', function() {
+		var e = [];
+
+		while (itr.next()) {
+			e.push(itr.current());
+		}
+
+		expect(e).to.be.eql(elements);
+	});
+
+});
