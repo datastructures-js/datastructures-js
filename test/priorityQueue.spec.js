dd20d0c55c98d2eb11f72acf10408cb93bf5aec1
Eyas Ranjous <eyas@eyasranjous.info>
2015-06-22 02:31:06 -0700

    refactor test

--- test/priorityQueue.spec.js
+++ test/priorityQueue.spec.js
@@ -4,32 +4,61 @@ var expect = require('chai').expect,
 describe('priorityQueue test', function() {
 
     var pq = priorityQueue().export();
-    
-    it('should be empty initially', function() {
-        expect(pq.isEmpty()).to.be.equal(true);
-        expect(pq.dequeue()).to.be.equal(null);
-        expect(pq.front()).to.be.equal(null);
-        expect(pq.back()).to.be.equal(null);
-    });
-    
-    it('should queue elements', function() {
-        pq.enqueue('john', 2);
-        pq.enqueue('sam', 4);
-        pq.enqueue('samantha', 1);
-        pq.enqueue('rose', 7);
-        expect(pq.enqueue.bind(pq, 'ivan', 'test')).to.throw({
-            message: 'priority should be a number'
+
+    describe('when the queue is empty', function() {
+
+        it('.isEmpty() should return true', function() {
+            expect(pq.isEmpty()).to.be.equal(true);
         });
-        expect(pq.isEmpty()).to.be.equal(false);
+
+        it('.length() should return 0', function() {
+            expect(pq.length()).to.be.equal(0);
+        });
+
+        it('.front(), .back() and .dequeue() should return null', function() {
+            expect(pq.dequeue()).to.be.equal(null);
+            expect(pq.front()).to.be.equal(null);
+            expect(pq.back()).to.be.equal(null);
+        });
+
+        it('.enqueue() should queue elements', function() {
+            pq.enqueue('john', 2);
+            pq.enqueue('sam', 4);
+            pq.enqueue('samantha', 1);
+            pq.enqueue('rose', 7);
+        });
+
+        it('.enqueue() should throw an exception wheen priority is not a number', function() {
+            expect(pq.enqueue.bind(pq, 'ivan', 'test')).to.throw({
+                message: 'priority should be a number'
+            });
+        });
+   
     });
 
-    it('should retreive front and back elements', function(){
-        expect(pq.front()).to.be.equal('john');
-        expect(pq.back()).to.be.equal('rose');
-    });
+    describe('when the queue is not empty', function() {
 
-    it('should dequeue an element', function() {
-        expect(pq.dequeue()).to.be.equal('samantha');
+        it('.isEmpty() should return false', function() {
+            expect(pq.isEmpty()).to.be.equal(false);
+        });
+
+        it('.length() should return a number bigger than 0', function() {
+            expect(pq.length()).to.be.equal(4);
+        });
+
+        it('.front() should retreive the front element', function(){
+            expect(pq.front()).to.be.equal('john');
+        });
+
+        it('.back() should retreive the back element', function(){
+            expect(pq.back()).to.be.equal('rose');
+        });
+
+        it('.dequeue() should dequeue a high priority element', function() {
+            expect(pq.dequeue()).to.be.equal('samantha');
+            expect(pq.length()).to.be.equal(3);
+        });
+
     });
 
 });
