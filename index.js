1f2a2e05325b28c01aee26e15e1623460e308491
Eyas Ranjous <eyas@eyasranjous.info>
2015-12-20 21:11:09 -0600

    refactor main module index

--- index.js
+++ index.js
@@ -9,45 +9,48 @@
 
 module.exports = (function() {
 
+    var self = {};
+
     var getObject = function(ds, args) {
         return require('lib/' + ds).apply(this, args).export();
     };
 
-    return  {
-        stack: function() {
-            return getObject('stack');
-        },
-
-        queue: function() {
-            return getObject('queue');
-        },
-
-        priorityQueue: function() {
-            return getObject('priorityQueue');
-        },
-
-        set: function() {
-            return getObject('set');
-        },
-
-        linkedList: function() {
-            return getObject('linkedList');
-        },
-
-        doublyLinkedList: function() {
-            return getObject('doublyLinkedList');
-        },
-
-        hashtable: function() {
-            return getObject('hashtable');
-        },
-
-        binarySearchTree: function() {
-            return getObject('binarySearchTree');
-        },
-
-        directedGraph: function() {
-            return getObject('directedGraph');
-        }
+    self.stack = function() {
+        return getObject('stack');
     };
+
+    self.queue = function() {
+        return getObject('queue');
+    };
+
+    self.priorityQueue = function() {
+        return getObject('priorityQueue');
+    };
+
+    self.set = function() {
+        return getObject('set');
+    };
+
+    self.linkedList = function() {
+        return getObject('linkedList');
+    };
+
+    self.doublyLinkedList = function() {
+        return getObject('doublyLinkedList');
+    };
+
+    self.hashtable = function() {
+        return getObject('hashtable');
+    };
+
+    self.binarySearchTree = function() {
+        return getObject('binarySearchTree');
+    };
+
+    self.directedGraph = function() {
+        return getObject('directedGraph');
+    };
+
+    return self;
+
 })();
