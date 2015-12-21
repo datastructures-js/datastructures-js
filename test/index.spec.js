48a41c3ec0e1a69f1bb463d28a05795288deed87
Eyas Ranjous <eyas@eyasranjous.info>
2015-12-20 21:26:53 -0600

    add test to the main module

---
+++ test/index.spec.js
@@ -0,0 +1,43 @@
+describe('index (main module) test', function() {
+
+    'use strict';
+
+    var expect = require('chai').expect,
+        ds = require('../index');
+
+    it('should get a stack object', function() {
+        expect(ds.stack()).to.be.an('object');
+    });
+
+    it('should get a queue object', function() {
+        expect(ds.queue()).to.be.an('object');
+    });
+
+    it('should get a priorityQueue object', function() {
+        expect(ds.priorityQueue()).to.be.an('object');
+    });
+
+    it('should get a set object', function() {
+        expect(ds.set()).to.be.an('object');
+    });
+
+    it('should get a linkedList object', function() {
+        expect(ds.linkedList()).to.be.an('object');
+    });
+
+    it('should get a doublyLinkedList object', function() {
+        expect(ds.doublyLinkedList()).to.be.an('object');
+    });
+
+    it('should get a hashtable object', function() {
+        expect(ds.hashtable()).to.be.an('object');
+    });
+
+    it('should get a binarySearchTree object', function() {
+        expect(ds.binarySearchTree()).to.be.an('object');
+    });
+
+    it('should get a directedGraph object', function() {
+        expect(ds.directedGraph()).to.be.an('object');
+    });
+});
