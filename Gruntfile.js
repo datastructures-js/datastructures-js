ed5de3f90966846eb258272fbe1ac9b4a0f4419f
Eyas Ranjous <eyas.ranjous@gmail.com>
2018-07-18 13:19:03 -0500

    initial files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,44 +1,27 @@
-/**
- * datastructures-js/Gruntfile
- * @class
- * @copyright 2018 Eyas Ranjous <eyas.ranjous@gmail.com>
- * @license MIT
- */
-
-class Gruntfile {
-
-  init(grunt) {
-    grunt.initConfig({
-      jshint: {
+module.exports = (grunt) => {
+  grunt.initConfig({
+    eslint: {
+      src: ['./*.js', './*.spec.js']
+    },
+    mochaTest: {
+      files: ['./*.spec.js']
+    },
+    mocha_istanbul: {
+      coverage: {
+        src: './',
         options: {
-          reporter: require('jshint-stylish'),
-          jshintrc: '.jshintrc'
-        },
-        files: ['./lib/**/*.js', './test/**/*.spec.js', './*.js']
-      },
-      mochaTest: {
-        files: ['./test/**/*.spec.js']
-      },
-      mocha_istanbul: {
-        coverage: {
-          src: 'test',
-          options: {
-            mask: '**/*.spec.js'
-          }
+          mask: '*.spec.js'
         }
       }
-    });
+    }
+  });
 
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-mocha-test');
-    grunt.loadNpmTasks('grunt-mocha-istanbul');
+  grunt.loadNpmTasks('grunt-eslint');
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-mocha-istanbul');
 
-    grunt.registerTask('lint', ['jshint']);
-    grunt.registerTask('test', ['mochaTest']);
-    grunt.registerTask('coverage', ['mocha_istanbul']);
-    grunt.registerTask('build', ['lint', 'coverage']);
-  }
-
-}
-
-module.exports = (new Gruntfile()).init;
+  grunt.registerTask('lint', ['eslint']);
+  grunt.registerTask('test', ['mochaTest']);
+  grunt.registerTask('coverage', ['mocha_istanbul']);
+  grunt.registerTask('build', ['lint', 'coverage']);
+};
