63879092a01756d39c1843d8716c17d355d69eb7
Eyas Ranjous <eyas.ranjous@gmail.com>
2022-07-13 19:55:58 -0700

    upgrade

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const { Queue } = require('@datastructures-js/queue');
 const { Stack } = require('@datastructures-js/stack');
+const { Deque } = require('@datastructures-js/deque');
+
 const { MinPriorityQueue, MaxPriorityQueue } = require('@datastructures-js/priority-queue');
+
 const { MinHeap, MaxHeap } = require('@datastructures-js/heap');
+
 const { EnhancedSet } = require('@datastructures-js/set');
+
 const {
   LinkedList,
   LinkedListNode,
   DoublyLinkedList,
   DoublyLinkedListNode
 } = require('@datastructures-js/linked-list');
+
 const { Trie, TrieNode } = require('@datastructures-js/trie');
+
 const { Graph, DirectedGraph } = require('@datastructures-js/graph');
+
 const {
   BinarySearchTree,
   BinarySearchTreeNode,
@@ -19,20 +27,29 @@ const {
 } = require('@datastructures-js/binary-search-tree');
 
 exports.Queue = Queue;
+
 exports.Stack = Stack;
+
+exports.Deque = Deque;
+
 exports.MinPriorityQueue = MinPriorityQueue;
 exports.MaxPriorityQueue = MaxPriorityQueue;
 exports.MinHeap = MinHeap;
 exports.MaxHeap = MaxHeap;
+
 exports.EnhancedSet = EnhancedSet;
+
 exports.LinkedList = LinkedList;
 exports.LinkedListNode = LinkedListNode;
 exports.DoublyLinkedList = DoublyLinkedList;
 exports.DoublyLinkedListNode = DoublyLinkedListNode;
+
 exports.Trie = Trie;
 exports.TrieNode = TrieNode;
+
 exports.Graph = Graph;
 exports.DirectedGraph = DirectedGraph;
+
 exports.BinarySearchTree = BinarySearchTree;
 exports.BinarySearchTreeNode = BinarySearchTreeNode;
 exports.AvlTree = AvlTree;
